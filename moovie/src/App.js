2aacc132a071739817e207b40958b139e1f034e8
Amos Mogaka <mogaka.amo254@gmail.com>
2021-01-24 09:55:39 +0300

    Commit

--- moovie/src/App.js
+++ moovie/src/App.js
@@ -1,3 +1,14 @@
 import React, {  } from "react";
 import ReactDOM, {  } from "react-dom";
 
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="title">Moovie</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render()
